7657db3947f7d1a4df9f5ca90638146f46d05c35
Gormartsen <gor@Gors-MacBook-Pro.local>
2014-07-18 11:29:09 -0500

    FIX js, mark read and styles

--- notices.js
+++ notices.js
@@ -1,26 +1,42 @@
-Drupal.behaviors.notice = function(context) {
-  $("div.notice-new a.noticeread").bind("click", function() {
-    var id = $(this).attr('rel');
-    $("#notice-" + id).removeClass('notice-new');
-    $("#noticeread-" + id).hide();
-    $("#notice-" + id + " div.noticeIcon img.new").hide();
-    
-    //submit ajax mark read
-    $.get('/ajax/notices/notices-mark-read/' + id);
-    return false;
-  });
-  $("div.notice a.noticeremove").bind("click", function() {
-    var id = $(this).attr('rel');
-    $("#notice-" + id).hide();
-    //submit ajax mark removed
-    $.ajax({
-      url: '/ajax/notices/notices-remove/' + id + '/' + $("#notices").attr('rel'),
-      dataType: 'json',      
-      type: "get",
-      success: function(data) {
-        $('#notices').append(data.data); 
-      }
-    });    
-    return false;
-  });
+/**
+ * @file
+ * Attaches behaviors for the Notices module.
+ */
+
+(function ($) {
+
+/**
+ * Implements Drupal.behaviors for the Notices module.
+ */
+ 
+Drupal.behaviors.notices  = {
+  attach: function (context, settings) {
+    $("div.notice-new a.noticeread").bind("click", function() {
+      var id = $(this).attr('rel');
+      $("#notice-" + id).removeClass('notice-new');
+      $("#noticeread-" + id).hide();
+      $("#notice-" + id + " div.noticeIcon img.new").hide();
+      
+      //submit ajax mark read
+      $.get('/ajax/notices/notices-mark-read/' + id);
+      return false;
+    });
+    $("div.notice a.noticeremove").bind("click", function() {
+      var id = $(this).attr('rel');
+      $("#notice-" + id).toggle("slow");
+      //submit ajax mark removed
+      $.ajax({
+        url: '/ajax/notices/notices-remove/' + id + '/' + $("#notices").attr('rel'),
+        dataType: 'json',      
+        type: "get",
+        success: function(data) {
+          $('#notices').append(data.data); 
+        }
+      });
+      return false;
+    });
+  },
+
 };
+
+})(jQuery);
