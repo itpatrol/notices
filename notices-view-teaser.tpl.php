ee28a07751e6a296550a1f837717e57a872374a4
Gormartsen <gor@Gors-MacBook-Pro.local>
2014-07-18 22:03:07 -0500

    Fix data styling

--- notices-view-teaser.tpl.php
+++ notices-view-teaser.tpl.php
@@ -1,16 +1,17 @@
 <div class="notice <?php print $zebra; ?><?php if ($notice->new) { print " notice-new";}?>" id="notice-<?php print $notice->noticeid; ?>">
+    <div class="picture"><img src="/<?php print $picture?>"> </div>
     <div class="date">
       <?php print $timeago; ?>
     </div>
-    <div class="picture"><img src="/<?php print $picture?>"> </div>
     <div class="message">
       <?php if ($notice->new) : ?>
         <span class="new"><?php print t('new');?></span>
       <?php endif ?>
       <?php print $content; ?>
-    </div>
     <?php if ( isset($links)) : ?>
        <?php print $links ?>
     <?php endif ?>
+
+    </div>
   <div class="clear-both bottom-border"></div>
 </div>
