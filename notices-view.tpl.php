29087b460f8bf6699ba2f5077b9ba9e10305cb0f
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2014-07-21 10:39:45 +0300

    FOUND 5 ERRORS AFFECTING 3 LINES
    1 | ERROR | Missing file doc comment
    1 | ERROR | The control statement should use the ":" alternative syntax
    |       | instead of curly braces in template files
    1 | ERROR | There should be no white space after an opening "{"
    12 | ERROR | There should be no white space after an opening "("
    16 | ERROR | Files must end in a single new line character

--- notices-view.tpl.php
+++ notices-view.tpl.php
@@ -1,16 +1,16 @@
-<div class="notice notice-view<?php if ($notice->new) { print " notice-new";}?>" id="notice-<?php print $notice->noticeid; ?>">
-    <div class="date">
-      <?php print $timeago; ?>
-    </div>
-    <div class="picture"><img src="/<?php print $picture?>"> </div>
-    <div class="message">
-      <?php if ($notice->new) : ?>
-        <span class="new"><?php print t('new');?></span>
-      <?php endif ?>
-      <?php print $content; ?>
-    </div>
-    <?php if ( isset($links)) : ?>
-       <?php print $links ?>
+<div class="notice notice-view<?php if ($notice->new): ?> <?php print ' notice-new'; ?> <?php endif; ?>" id="notice-<?php print $notice->noticeid; ?>">
+  <div class="date">
+    <?php print $timeago; ?>
+  </div>
+  <div class="picture"><img src="/<?php print $picture?>"> </div>
+  <div class="message">
+    <?php if ($notice->new): ?>
+      <span class="new"><?php print t('new');?></span>
     <?php endif ?>
+    <?php print $content; ?>
+  </div>
+  <?php if (isset($links)): ?>
+    <?php print $links ?>
+  <?php endif ?>
   <div class="clear-both bottom-border"></div>
 </div>
