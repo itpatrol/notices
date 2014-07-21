f5620bce8162ab3a812a5a0ffadc0189a74c5fe3
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2014-07-21 10:51:17 +0300

    Fix 3 ERRORS in file "notices-view-teaser.tpl.php".
    1 | ERROR | The control statement should use the ":" alternative syntax
    |       | instead of curly braces in template files
    1 | ERROR | There should be no white space after an opening "{"
    12 | ERROR | There should be no white space after an opening "("

--- notices-view-teaser.tpl.php
+++ notices-view-teaser.tpl.php
@@ -1,15 +1,15 @@
-<div class="notice <?php print $zebra; ?><?php if ($notice->new) { print " notice-new";}?>" id="notice-<?php print $notice->noticeid; ?>">
+<div class="notice <?php print $zebra; ?><?php if ($notice->new): ?> <?php print " notice-new"; ?> <?php endif; ?>" id="notice-<?php print $notice->noticeid; ?>">
     <div class="picture"><img src="/<?php print $picture?>"> </div>
     <div class="date">
       <?php print $timeago; ?>
     </div>
     <div class="message">
-      <?php if ($notice->new) : ?>
+      <?php if ($notice->new): ?>
         <span class="new"><?php print t('new');?></span>
       <?php endif ?>
       <?php print $content; ?>
     </div>
-    <?php if ( isset($links)) : ?>
+    <?php if (isset($links)): ?>
        <?php print $links ?>
     <?php endif ?>
   <div class="clear-both bottom-border"></div>
